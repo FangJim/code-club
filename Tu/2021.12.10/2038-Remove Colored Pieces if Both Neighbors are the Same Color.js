98d8d3ffc0285a807786d45940789f9f1594d1ff
Tu-yunhsuan <cindy995533@gmail.com>
2021-12-11 22:48:30 +0800

    update

--- Tu/2021.12.10/2038-Remove Colored Pieces if Both Neighbors are the Same Color.js
+++ Tu/2021.12.10/2038-Remove Colored Pieces if Both Neighbors are the Same Color.js
@@ -11,16 +11,14 @@
  var winnerOfGame = function(colors) {
     let countA=0;
     let countB=0;
-    if(colors.length<3){return false;}
-    for(let i=1; i<colors.length-1; i++){
-        if(colors[i]==="A" && colors[i-1]==="A" && colors[i+1]==="A"){
-            countA++;
-        }
-        if(colors[i]==="B" && colors[i-1]==="B" && colors[i+1]==="B"){
-            countB++;
+    let num=colors.length;
+    if(num<3){return false;}
+    for(let i=1; i<num-1; i++){
+        if(colors[i]==colors[i-1] && colors[i]===colors[i+1]){
+            if (colors[i]==="A"){countA++;}
+            else{countB++;}
         }
     }
-    if(countA>countB){return true;}
-    else{return false;}
+    return countA>countB
     //Alice為先移動者，當countA==countB時，下一輪順序的Alice已無法移動->lose
 };
