ff799b38528ef235360c71e5786389d8026d3f4f
Tu-yunhsuan <cindy995533@gmail.com>
2021-12-13 00:48:00 +0800

    2021/12/12

--- Tu/2021.12.17/880-Decoded String at Index.js
+++ Tu/2021.12.17/880-Decoded String at Index.js
@@ -3,6 +3,26 @@
  * @param {number} k
  * @return {string}
  */
- var decodeAtIndex = function(s, k) {
-    
+//a~z的編碼為97~122
+var decodeAtIndex = function(s, k) {
+    let str = "";
+    let tmp = "";
+    for(let i=0; i<s.length; i++){
+        if(s.charCodeAt(i)>96 && s.charCodeAt(i)<123){  
+            //如果是字母
+            str+=s[i];
+        }
+        else if(s[i]>0 && s[i]<=9){   
+            //如果是數字
+            tmp = str;
+            str += tmp.repeat(s[i]-1);
+        }
+        
+        if(str.length>k){
+            return str[k-1];
+        }
+    }
+    // return tmp;
+    // return str;
+    return str[k-1];
 };
--- Tu/2021.12.17/BrokenKeyboard.js
+++ Tu/2021.12.17/BrokenKeyboard.js
@@ -1,1 +1,34 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var Beiju = function(s) {
+    let str="";     //儲存字串
+    let brackets=1; //判斷是否有括號，0代表左括號，1代表右括號。
+    let index=0;    //判斷索引值
+    for(let i=0; i<s.length; i++){
+        if(s[i]=="["){
+            index=0;
+            brackets=0;
+        }else if(s[i]=="]"){
+            brackets=1;
+        }else{
+            if(brackets==1){        //當brackets==1時，將letter加在最後面
+                str += s[i];
+            }
+            else if(brackets==0){   //當brackets==0時，將letter加在最前面
+                str = str.slice(0,index) + s[i] + str.slice(index);   //切割字串後再相加
+                index++;
+            }
+        }
+    }
+    return str;
+};
+
 //test
+let a="This_is_a_[Beiju]_text";
+let b="[[]][][]Happy_Birthday_to_Tsinghua_University";
+let c="[[]][]world[hello_]_!";
+console.log(Beiju(a));
+console.log(Beiju(b));
+console.log(Beiju(c));
