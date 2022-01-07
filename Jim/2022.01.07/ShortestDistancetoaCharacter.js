89e4d9811ac496019c518c8d40f0bc9adadf177b
Jim Fang <jmjmjim1101@gmail.com>
2022-01-07 19:27:01 +0800

    Update new questions

--- Jim/2022.01.07/ShortestDistancetoaCharacter.js
+++ Jim/2022.01.07/ShortestDistancetoaCharacter.js
@@ -3,47 +3,49 @@
  * @param {character} c
  * @return {number[]}
  */
-// let shortestToChar_badWay = (s, c) => {
-//     let s = s.split('');
-//     let cPosition = [];
-//     let ans = [];
-//     for (let i = 0; i < s.length; i++) {
-//         if (s[i] === c) {
-//             cPosition.push(i);
+let shortestToChar_badWay = (string, c) => {
+    let s = string.split('');
+    let cPosition = [];
+    let ans = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === c) {
+            cPosition.push(i);
+        }
+    }
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === c) ans.push(0);
+        else {
+            let min = Number.POSITIVE_INFINITY;
+            console.log(min)
+            for (let j = 0; j < cPosition.length; j++) {
+                if (Math.abs(cPosition[j] - i) < min) {
+                    min = Math.abs(cPosition[j] - i)
+                }
+                else break
+            }
+            ans.push(min)
+        }
+    }
+    return ans
+};
+
+// let shortestToChar = (s, c) => {
+//     let n = s.length
+//     let ans = new Array(n)
+//     let c_position = -n;
+//     for (let i = 0; i < n; i++) {//往右校正
+//         if (s[i] == c) {
+//             c_position = i
 //         }
+//         ans[i] = i - c_position
 //     }
-//     for (let i = 0; i < s.length; i++) {
-//         if (s[i] === c) ans.push(0);
-//         else {
-//             let min = Number.POSITIVE_INFINITY;
-//             for (let j = 0; j < cPosition.length; j++) {
-//                 if (Math.abs(cPosition[j] - i) < min) {
-//                     min = Math.abs(cPosition[j] - i)
-//                 }
-//                 else break
-//             }
-//             ans.push(min)
+//     for (let i = n - 1; i >= 0; i--) {//往左校正
+//         if (s[i] == c) {
+//             c_position = i
 //         }
+//         ans[i] = Math.min(ans[i], Math.abs(i - c_position))
 //     }
 //     return ans
 // };
 
-
-let shortestToChar = (s, c) => {
-    let n = s.length
-    let ans = new Array(n)
-    let c_position = -n;
-    for (let i = 0; i < n; i++) {//往右校正
-        if (s[i] == c) {
-            c_position = i
-        }
-        ans[i] = i - c_position
-    }
-    for (let i = n - 1; i >= 0; i--) {//往左校正
-        if (s[i] == c) {
-            c_position = i
-        }
-        ans[i] = Math.min(ans[i], Math.abs(i - c_position))
-    }
-    return ans
-}
+console.log(shortestToChar_badWay("loveleetcode", "e"))
