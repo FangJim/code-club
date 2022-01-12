aa1c8cea95d3cfd662980cf71e5bed0623039fc0
Jim Fang <jmjmjim1101@gmail.com>
2022-01-12 21:42:13 +0800

    Update

--- 方子樽/2022.01.21/ProductofArrayExceptSelf.js
+++ 方子樽/2022.01.21/ProductofArrayExceptSelf.js
@@ -1,12 +1,22 @@
 /**
+ * 1. 計算該數左邊的乘積 紀錄於ans
+ * 2. 計算該數右邊的乘積 並乘該位置ans之值
  * @param {number[]} nums
  * @return {number[]}
  */
 let productExceptSelf = (nums) => {
-    let left = []
-    let right = []
-    let ans
-    for (let i = 0; i < nums.length; i++) {
-
+    let length = nums.length
+    let temp = 1
+    let ans = []
+    for (let i = 0; i < length; i++) {
+        ans[i] = temp
+        temp *= nums[i]
     }
+    temp = 1
+    for (let i = length - 1; i >= 0; i--) {
+        ans[i] *= temp
+        temp *= nums[i]
+    }
+    return ans
 };
+console.log(productExceptSelf([1, 2, 3, 4]))
