9797929e04114d6cabb3da4dc2690a16e2a59368
Chrissy1209 <chrissy93881209@gmail.com>
2022-01-21 15:27:51 +0800

    20220121

---
+++ 許靜玟/2021.11.19/TwoSum.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+******************************************************************************/
+#include <iostream> //2022.0116
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> nums; //動態陣列用於儲存使用者輸入的數字陣列
+    int index; //輸入想儲存的陣列長度
+    cin >> index;
+    while(index != 0)
+    {
+        int x;
+        cin >> x;
+        nums.push_back(x); //依序輸入陣列內數字並存入nums
+        index--;
+    }
+    int target;
+    cin >> target; //欲尋找的數字總和target
+    for(auto a : nums) // if nums = [1, 2, 3], target = 4
+    {
+        for(int i = index; i < nums.size()-1; i++) //nums[index] + nums[index+1] (1 + 2) and nums[index] + nums[index+2] (1 + 3)
+        {
+            if(a + nums[i+1] == target) 
+            {
+                cout << target << " = index " << index << " + index " << i+1 ;
+                break;
+            }
+        }
+        index++; //index 表示a in nums的位置
+    }
+}
+
