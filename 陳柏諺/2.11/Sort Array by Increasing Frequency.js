d408b433deba5afb6ae637b9f833ef73263889cd
Yan <mm7246591@yahoo.com.tw>
2022-02-06 17:30:52 +0800

    2/6

---
+++ 陳柏諺/2.11/Sort Array by Increasing Frequency.js
@@ -0,0 +1,23 @@
+/**
+ * 1. 先算出這個陣列重複的值有幾個
+ * 2. 再用sort去排序
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const frequencySort = function(nums) {
+    const arry = nums.reduce((obj, index) => {
+        if (!obj[index]) {
+            obj[index] = 1;
+        } else {
+            obj[index] += 1;
+        }
+        return obj
+    }, {})
+    return nums.sort(function(a, b) {
+        if (arry[a] == arry[b]) {
+            return b - a;
+        } else {
+            return arry[a] - arry[b];
+        }
+    });
+};
