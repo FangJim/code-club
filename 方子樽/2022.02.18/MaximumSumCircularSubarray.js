54e3b6cfcf0c28b54f60c7748aee241083746e35
fang <fang@fangde-MacBook-Air.local>
2022-02-17 18:47:43 +0800

    Update

--- 方子樽/2022.02.18/MaximumSumCircularSubarray.js
+++ 方子樽/2022.02.18/MaximumSumCircularSubarray.js
@@ -1,7 +1,15 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-const maxSubarraySumCircular = (nums) => {
+const maxSubarraySumCircular = (arr) => {
+    let [curMax, curMin, gloMax, gloMin] = new Array(4).fill(arr[0]);
+    let sum = arr.reduce((pre, cur) => pre + cur, 0);
 
+    for (let i = 1; i < arr.length; i++) {
+        curMax = Math.max(arr[i], arr[i] + curMax);
+        gloMax = Math.max(gloMax, curMax);
+
+        curMin = Math.min(arr[i], arr[i] + curMin);
+        gloMin = Math.min(gloMin, curMin);
+    }
+
+    return (sum === gloMin) ? gloMax : Math.max(gloMax, sum - gloMin);
 };
+console.log(maxSubarraySumCircular([5, -3, 5]));
